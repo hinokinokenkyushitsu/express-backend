5a0cefc0f37590fa60068f659a04732e3674b541
Hinoki <hinokinokenkyushitsu@gmail.com>
2026-05-10 21:16:56 +0800

    Update JwtAuthFilter.java

--- src/main/java/com/fiveok/express/base/config/JwtAuthFilter.java
+++ src/main/java/com/fiveok/express/base/config/JwtAuthFilter.java
@@ -38,9 +38,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain chain) throws ServletException, IOException {
-        
+
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-        response.setStatus(HttpServletResponse.SC_OK);
+        chain.doFilter(request, response);
         return;
         }
 
